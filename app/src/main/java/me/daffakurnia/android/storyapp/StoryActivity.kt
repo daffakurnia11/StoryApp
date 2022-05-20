2af313ff4a79af08aa9203842675458b4dac4b60
daffakurnia11 <72376750+daffakurnia11@users.noreply.github.com>
2022-05-20 21:06:31 +0700

    Creating Intent Camera for adding new story

--- app/src/main/java/me/daffakurnia/android/storyapp/StoryActivity.kt
+++ app/src/main/java/me/daffakurnia/android/storyapp/StoryActivity.kt
@@ -1,12 +1,21 @@
 package me.daffakurnia.android.storyapp
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import me.daffakurnia.android.storyapp.databinding.ActivityStoryBinding
+import java.io.File
 
 class StoryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStoryBinding
+    private lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +23,32 @@ class StoryActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.title = resources.getString(R.string.add_story)
+
+        binding.buttonCamera.setOnClickListener { startTakePhoto() }
+    }
+
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@StoryActivity,
+                "me.daffakurnia.android.storyapp",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            val result = BitmapFactory.decodeFile(myFile.path)
+            binding.previewImageView.setImageBitmap(result)
+        }
     }
 }
